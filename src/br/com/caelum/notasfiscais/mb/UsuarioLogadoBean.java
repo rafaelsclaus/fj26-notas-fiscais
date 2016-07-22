a896714bafad6d48b58306f655f869015544d14f
rafaelsclaus <rafael_claus@hotmail.com>
2016-07-21 21:24:02 -0300

    Finalizando a coesao de usuarios.

---
+++ src/br/com/caelum/notasfiscais/mb/UsuarioLogadoBean.java
@@ -0,0 +1,32 @@
+package br.com.caelum.notasfiscais.mb;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import br.com.caelum.notasfiscais.modelo.Usuario;
+
+@Named @SessionScoped
+public class UsuarioLogadoBean implements Serializable {
+
+	private Usuario usuario;
+	
+	public void logar(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	public void deslogar() {
+		this.usuario = null;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+}
