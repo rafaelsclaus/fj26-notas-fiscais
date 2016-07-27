be7cd543b2779c392206ab91bc7d253ea474bd8b
rafaelsclaus <rafael_claus@hotmail.com>
2016-07-26 23:01:38 -0300

    Implementando Observer.

--- src/br/com/caelum/notasfiscais/mb/LoginBean.java
+++ src/br/com/caelum/notasfiscais/mb/LoginBean.java
@@ -3,6 +3,7 @@ package br.com.caelum.notasfiscais.mb;
 import java.io.Serializable;
 
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -20,10 +21,14 @@ public class LoginBean implements Serializable{
 	@Inject
 	private UsuarioLogadoBean usuarioLogadoBean;
 	
+	@Inject
+	Event<Usuario> eventoLogin;
+	
 	public String efetuaLogin() {
 
 		boolean loginValido = dao.existe(this.usuario);
 		if (loginValido) {
+			eventoLogin.fire(usuario);
 			usuarioLogadoBean.logar(usuario);
 			return "produto?faces-redirect=true";
 		}else {
