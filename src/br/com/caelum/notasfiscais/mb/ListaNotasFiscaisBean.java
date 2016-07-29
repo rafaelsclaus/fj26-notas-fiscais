9581201015ded1f32688ffbcbf9a04517a5923ef
rafaelsclaus <rafael_claus@hotmail.com>
2016-07-28 22:55:52 -0300

    Implementando Temas.

--- src/br/com/caelum/notasfiscais/mb/ListaNotasFiscaisBean.java
+++ src/br/com/caelum/notasfiscais/mb/ListaNotasFiscaisBean.java
@@ -1,5 +1,6 @@
 package br.com.caelum.notasfiscais.mb;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.view.ViewScoped;
@@ -7,18 +8,24 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.caelum.notasfiscais.dao.NotaFiscalDao;
+import br.com.caelum.notasfiscais.datamodel.DataModelNotasFiscais;
 import br.com.caelum.notasfiscais.modelo.NotaFiscal;
 
 @ViewScoped
 @Named
-public class ListaNotasFiscaisBean {
+public class ListaNotasFiscaisBean implements Serializable{
 	
 	@Inject
 	private NotaFiscalDao dao;
+	
+	@Inject
+	private DataModelNotasFiscais dataModel;
 	
 	public List<NotaFiscal> getNotasFiscais() {
 		return dao.listaTodos();
 	}
 	
-	
+	public DataModelNotasFiscais getDataModel() {
+		return dataModel;
+	}
 }
